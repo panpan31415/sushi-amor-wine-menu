0ee9582a4ebfe6aaf4b4e858f2f760506bf65fdc
panpan31415 <panpan31415@gmail.com>
2022-09-29 14:57:56 +0200

    elborate tea data

--- src/teas.js
+++ src/teas.js
@@ -3,15 +3,17 @@ const teas = {
     image1Url: "images/teas/T1_1.webp",
     image2Url: "images/teas/T1_2.webp",
     name: "Grøn Gojibær Te",
-    ingredient: "Grøn the, Gojibær, Citrongræs, Granatæble, Aroma, Kornblomst",
+    ingredient: "Grøn the, Gojibær, Citrongræs, Granatæble, Aroma, Kornblomst.",
     description:
       "Klassisk mild og blød økologisk grøn Sencha te fra Kina kombineret med skønne japanske gojibær, frisk granatæble, søde blåbær samt citrongræs og kornblomst.",
+    systemLabel: "T1",
   },
   T2: {
     image1Url: "images/teas/T2_1.webp",
     image2Url: "images/teas/T2_2.webp",
     name: "Æble Te, Økologisk",
-    ingredient: "Sort the, Æblestykker, Naturlig æble aroma",
+    ingredient: "Sort the, Æblestykker, Naturlig æble aroma.",
+    systemLabel: "T2",
     description:
       "Sort økologisk Keemun te og økologiske æblestykker af højeste kvalitet. Keemun te er kendt som en af de mildere sorte teer og derfor er indholdet af garvesyre ret begrænset. Tilføj dertil æblestykker og du får den perfekte eftermiddagste.",
   },
@@ -19,7 +21,8 @@ const teas = {
     image1Url: "images/teas/T3_1.webp",
     image2Url: "images/teas/T3_2.webp",
     name: "Grøn Sakura Te (Japansk Kirsebær)",
-    ingredient: "Grøn the, Kirsebær aroma, Rosenblade",
+    ingredient: "Grøn the, Kirsebær aroma, Rosenblade.",
+    systemLabel: "T3",
     description:
       "Grøn Sakura te er sammensat af den milde og klassiske grønne Sencha te, japanske sakura kirsebær samt rosenblade. En skøn kop der smager af forår.",
   },
@@ -28,7 +31,8 @@ const teas = {
     image2Url: "images/teas/T4_2.webp",
     name: "Kinesisk Forårs Te (Chinese Spring Tea)",
     ingredient:
-      "Sort Ceylon the, Grøn the, Kirsebær, Rosenblade, Jasmin blomster, Aroma",
+      "Sort Ceylon the, Grøn the, Kirsebær, Rosenblade, Jasmin blomster, Aroma.",
+    systemLabel: "T4",
     description:
       "Kinesisk Forårste er en skøn blanding med en herlig duft af forår. Den er sammensat af både grøn og sort te og er en af smagfulde blandinger. Klassisk Ceylon te kombineret med grøn te giver dig en let og alligevel fyldig kop te. Tilføj derudover rose, jasmin, kirsebær og bergamotte, så får du en fantastisk teoplevelse.",
   },
